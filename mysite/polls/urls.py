5de4059e0bda9fca0258ddc1b800b55732fc9edd
Pav125 <devipavan824@gmail.com>
2024-03-27 16:27:29 +0530

    converted views to generic views and updated urls

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,9 +4,22 @@ from . import views
 
 app_name = 'polls'
 
+# urlpatterns = [
+#     path('', views.index, name = 'index'),
+#     path('<int:question_id>/', views.detail, name = 'detail'),
+#     path('<int:question_id>/results/', views.results, name = 'results'),
+#     path('<int:question_id>/vote/', views.vote, name = 'vote')
+# ]
+
+# Amend URLconf
 urlpatterns = [
-    path('', views.index, name = 'index'),
-    path('<int:question_id>/', views.detail, name = 'detail'),
-    path('<int:question_id>/results/', views.results, name = 'results'),
+    path('', views.IndexView.as_view(), name = 'index'),
+    path('<int:pk>/', views.DetailView.as_view(), name = 'detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name = 'results'),
     path('<int:question_id>/vote/', views.vote, name = 'vote')
 ]
+
+'''Note that the name of the matched pattern in the path strings of the second and third patterns 
+has changed from <question_id> to <pk>. This is necessary because we’ll use the DetailView 
+generic view to replace our detail() and results() views, and it expects the primary key value 
+captured from the URL to be called "pk".'''
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from django.db.models import F
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question,Choice
 
@@ -10,41 +11,57 @@ from .models import Question,Choice
  object containing the content for the rquested page, or raising an exception such as 
  Http404.'''
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # output = ', '.join([q.question_text for q in latest_question_list]) #1 method
-    # template = loader.get_template('polls/index.html') #2 method
-    '''That code loads the template called polls/index.html and passes it a context. 
-    The context is a dictionary mapping template variable names to Python objects.'''
-    context = {
-        'latest_question_list' : latest_question_list,
-    }
-    # return HttpResponse(template.render(context, request)) #2
-    return render(request, 'polls/index.html', context)
-    ''''''
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     # output = ', '.join([q.question_text for q in latest_question_list]) #1 method
+#     # template = loader.get_template('polls/index.html') #2 method
+#     '''That code loads the template called polls/index.html and passes it a context. 
+#     The context is a dictionary mapping template variable names to Python objects.'''
+#     context = {
+#         'latest_question_list' : latest_question_list,
+#     }
+#     # return HttpResponse(template.render(context, request)) #2
+#     return render(request, 'polls/index.html', context)
 
-def detail(request, question_id):
-    # try:
-    #     question = Question.objects.get(pk = question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404('Question deoes not exist')
-    # The view raises the Http404 exception if a question with the requested ID doesn’t exist.
+#Amend Views
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
     
-    # return HttpResponse("You're looking at question %s." %question_id)
-    question = get_object_or_404(Question, pk = question_id)
-    return render(request, 'polls/detail.html', { 'question' : question })
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, question_id):
-    # response = "You're looking at results of question %s."
-    # return HttpResponse(response %question_id)
-    question = get_object_or_404(Question, pk = question_id)
-    return render(
-        request,
-        'polls/results.html',
-        {
-            'question' : question
-        }
-    )
+# def detail(request, question_id):
+#     # try:
+#     #     question = Question.objects.get(pk = question_id)
+#     # except Question.DoesNotExist:
+#     #     raise Http404('Question deoes not exist')
+#     # The view raises the Http404 exception if a question with the requested ID doesn’t exist.
+    
+#     # return HttpResponse("You're looking at question %s." %question_id)
+    
+#     question = get_object_or_404(Question, pk = question_id)
+#     return render(request, 'polls/detail.html', { 'question' : question })
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+# def results(request, question_id):
+#     # response = "You're looking at results of question %s."
+#     # return HttpResponse(response %question_id)
+#     question = get_object_or_404(Question, pk = question_id)
+#     return render(
+#         request,
+#         'polls/results.html',
+#         {
+#             'question' : question
+#         }
+#     )
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
     
 def vote(request, question_id):
     # return HttpResponse("You're voting on question %s." %question_id)
