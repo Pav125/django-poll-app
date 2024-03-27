2fff425286e0b9de467e86d11ad3d22b1858e1da
Pav125 <devipavan824@gmail.com>
2024-03-27 15:55:06 +0530

    addes results.html and updated views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
+from django.db.models import F
+from django.urls import reverse
 
-from .models import Question
+from .models import Question,Choice
 
 '''Each view is responsible for doing one of two things: returning an HttpResponse 
  object containing the content for the rquested page, or raising an exception such as 
@@ -30,14 +32,44 @@ def detail(request, question_id):
     
     # return HttpResponse("You're looking at question %s." %question_id)
     question = get_object_or_404(Question, pk = question_id)
-    return render(request, 'polls/detail.html', { 'question' : question})
+    return render(request, 'polls/detail.html', { 'question' : question })
 
 def results(request, question_id):
-    response = "You're looking at results of question %s."
-    return HttpResponse(response %question_id)
-
+    # response = "You're looking at results of question %s."
+    # return HttpResponse(response %question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    return render(
+        request,
+        'polls/results.html',
+        {
+            'question' : question
+        }
+    )
+    
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." %question_id)
+    # return HttpResponse("You're voting on question %s." %question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    try:
+        selected_choice = question.choice_set.get(pk = request.POST["choice"])
+    except(KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            'polls/detail.html',
+            {
+                'question' : question,
+                'error_message' : "You did'nt select a choice"
+            }
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args = (question.id,)))
+                                                  
+
+    
 
 '''There’s a problem here, though: the page’s design is hard-coded in the view. 
 If you want to change the way the page looks, you’ll have to edit this Python code. 
